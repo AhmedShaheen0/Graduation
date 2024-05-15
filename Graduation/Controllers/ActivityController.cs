e5b27dc2cd85a51a8a6941a65f610bf3503db69c
Ahmed Magdy <shetos3500@gmail.com>
2024-05-15 04:28:03 +0300

    edid

--- Graduation/Controllers/ActivityController.cs
+++ Graduation/Controllers/ActivityController.cs
@@ -12,7 +12,7 @@ using Graduation.Services.Activity;
 
 namespace Graduation.Controllers
 {
-    [Authorize(Roles = AppRoles.Admin + ", " + AppRoles.User)]
+    [Authorize(Roles = AppRoles.Admin  + AppRoles.User)]
     [ApiController]
     [Route("[controller]")]
     public class ActivityController : ControllerBase
